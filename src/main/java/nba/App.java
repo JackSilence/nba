d0f3fd7755ec2166a017a2d9d5e5d49080f3e9d2
JackSilence <jacksilence@gmail.com>
2019-05-24 12:04:25 +0800

    更新依賴

--- src/main/java/nba/App.java
+++ src/main/java/nba/App.java
@@ -6,11 +6,13 @@ import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import magic.controller.ExecuteController;
+import magic.service.AsyncExecutor;
 import magic.service.SendGrid;
+import magic.service.Slack;
 
 @SpringBootApplication
 @EnableScheduling
-@Import( { ExecuteController.class, SendGrid.class } )
+@Import( { ExecuteController.class, SendGrid.class, AsyncExecutor.class, Slack.class } )
 public class App {
 	public static void main( String[] args ) {
 		SpringApplication.run( App.class, args );
